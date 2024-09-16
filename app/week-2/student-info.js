eeb8f7fd5f8492d31be4226362babcb6cc7967db
shawncgy <166419310+shawnscgy@users.noreply.github.com>
2024-09-15 18:24:26 -0600

    Finished Assignment1
    Done

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+export default function StudentInf() {
+  return (
+    <header>
+      <h1>Name: Shawn Shi</h1>
+      <p>
+        GitHub:
+        <Link href="https://github.com/shawnscgy">shawnscgy.git</Link>
+      </p>
+    </header>
+  );
+}
