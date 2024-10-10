3b65e5723e17705ebdc260b778f7502c774587de
shawncgy <166419310+shawnscgy@users.noreply.github.com>
2024-10-09 23:55:15 -0600

    Update page.js

--- app/week-6/page.js
+++ app/week-6/page.js
@@ -1,7 +1,7 @@
-export default function Page() {
-  return (
-    <main className="flex justify-center">
-      <NewItem className="flex-1" />
-    </main>
-  );
-}
+// export default function Page() {
+//   return (
+//     <main className="flex justify-center">
+//       <NewItem className="flex-1" />
+//     </main>
+//   );
+// }
